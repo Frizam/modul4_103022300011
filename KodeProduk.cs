9024b52d4ff53a6805fb4630e0e3e43b602d3f1c
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:14:48 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300011
+{
+    internal class KodeProduk
+    {
+        public enum ProdukElektronik { //enum untuk merepresentasikan Produk Elektronik
+            Laptop,
+            Smartphone,
+            Tablet,
+            Headset,
+            Keyboard,
+            Mouse,
+            Printer,
+            Monitor,
+            Smartwatch,
+            Kamera
+        }
+
+        //Method untuk mengembalikan kode produk dari tabel produk elektronik diatas
+        public static string getKodeProduk(ProdukElektronik produkElektronik) {
+            string[] kodeProduk = { "E100", "E101", "E102", "E103", 
+                "E104", "E105", "E106", "E107", "E108", "E109" };
+            
+            return kodeProduk[(int)produkElektronik];
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul4_103022300011;
+using static modul4_103022300011.KodeProduk;
+
+Console.WriteLine("Masukan nama produk:");
+string input  = Console.ReadLine();
+
+//Untuk mengkonvert input string menjadi enum
+if (Enum.TryParse(input, out KodeProduk.ProdukElektronik produk))
+{
+    string kodeProduk = KodeProduk.getKodeProduk(produk);
+    Console.WriteLine("Kode Produk " + input + ": " + kodeProduk);
+}
+else {
+    Console.WriteLine("Kode Produk " + input + " Tidak ditemukan"); //jika konvertan input string tidak ada di enum
+}
