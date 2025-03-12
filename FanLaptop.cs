d945ad4a9a0c66946f3bf7bc911bbcf5fa5e6867
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 17:16:36 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_103022300011
+{
+    internal class FanLaptop
+    {
+        public enum FanState{
+            Quiet,
+            Balanced,
+            Performance,
+            Turbo
+        }
+        public FanState currentState;
+
+        public FanLaptop() { //untuk meng-set state awal jadi quiet
+            currentState = FanState.Quiet;
+            Console.WriteLine("Fan sedang Quiet");
+        }
+        // Method untuk Quiet mode
+        public void QuietMode()
+        {
+            if (currentState == FanState.Turbo)
+            {
+                Console.WriteLine("Turbo Shortcut..."); //Trigger
+                currentState = FanState.Quiet;
+                Console.WriteLine("Fan Turbo berubah menjadi Quiet");
+            }
+            else if (currentState == FanState.Balanced) {
+                Console.WriteLine("Mode Down...");//Trigger
+                currentState = FanState.Quiet;
+                Console.WriteLine("Fan Balanced berubah menjadi Quiet");
+            }
+            else
+            {
+                Console.WriteLine("Fan sudah Quiet");
+            }
+        }
+
+        // Method untuk Turbo mode
+        public void TurboMode()
+        {
+            if (currentState == FanState.Quiet)
+            {
+                Console.WriteLine("Turbo Shortcut...");//Trigger
+                currentState = FanState.Turbo;
+                Console.WriteLine("Fan Quiet berubah menjadi Turbo");
+            }
+            else if (currentState == FanState.Performance)
+            {
+                Console.WriteLine("Mode Up...");//Trigger
+                currentState = FanState.Turbo;
+                Console.WriteLine("Fan Performance berubah menjadi Turbo");
+            }
+            else
+            {
+                Console.WriteLine("Fan sudah Turbo");
+            }
+        }
+
+        // Method untuk Balanced mode
+        public void BalancedMode()
+        {
+            if (currentState == FanState.Quiet)
+            {
+                Console.WriteLine("Mode Up...");//Trigger
+                currentState = FanState.Balanced;
+                Console.WriteLine("Fan Quiet berubah menjadi Balanced");
+            }
+            else if (currentState == FanState.Performance)
+            {
+                Console.WriteLine("Mode Down...");//Trigger
+                currentState = FanState.Balanced;
+                Console.WriteLine("Fan Performance berubah menjadi Balanced");
+            }
+            else
+            {
+                Console.WriteLine("Fan sudah Balanced");
+            }
+        }
+    }   
+}
--- Program.cs
+++ Program.cs
@@ -8,9 +8,40 @@ string input  = Console.ReadLine();
 //Untuk mengkonvert input string menjadi enum
 if (Enum.TryParse(input, out KodeProduk.ProdukElektronik produk))
 {
-    string kodeProduk = KodeProduk.getKodeProduk(produk);
+    string kodeProduk = KodeProduk.getKodeProduk(produk); //untuk menampung kode produk dari produk yang diinput
     Console.WriteLine("Kode Produk " + input + ": " + kodeProduk);
 }
 else {
     Console.WriteLine("Kode Produk " + input + " Tidak ditemukan"); //jika konvertan input string tidak ada di enum
+}
+
+
+FanLaptop fan = new FanLaptop(); //di set menjadi quiet disini
+
+string command;
+
+while (true) {
+    Console.WriteLine("Masukan Perintah: ('Exit' untuk keluar program)");
+    command = Console.ReadLine();
+
+    if (command == "Exit")
+    {
+        Console.WriteLine("Selesai.");
+        break;
+    }
+    else if (command == "Quiet Mode")
+    {
+        fan.QuietMode(); //Quiet mode diaktifkan
+    }
+    else if (command == "Balanced Mode")
+    {
+        fan.BalancedMode(); //Balanced mode diaktifkan
+    }
+    else if (command == "Turbo Mode")
+    {
+        fan.TurboMode(); //Turbo mode diaktifkan
+    }
+    else {
+        Console.WriteLine("Input Perintah yang benar");
+    }
 }
